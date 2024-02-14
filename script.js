1a41951900dc999095fd86c3029c1f2c4056ae8c
PeaChack <17183132+PeaChack@users.noreply.github.com>
2024-02-14 11:57:57 +0300

    some changes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const button = document.getElementById("escapeButton");
 
-button.addEventListener("mousemove", (e) => {
-	const x = e.clientX / window.innerWidth - 0.5;
-	const y = e.clientY / window.innerHeight - 0.5;
-	
-	button.style.setProperty("--button-width", '${button.offsetWidth}px');
-	button.style.setProperty("--button-height", '${button.offsetHeight}px');
-	
-	button.style.transform = translate(${x * 25}px, ${y * 25}px);
+
+button.addEventListener("mouseover", (e) =>{
+	const button = document.getElementById("escapeButton");
+	console.log(button.style);
+	let x = window.innerWidth - parseInt(button.style.width, 10);
+	console.log(x);
+	button.style.left = x + 'px';
 });
