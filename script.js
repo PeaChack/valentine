3359dfd62c1d33ec6b5cfb68cdb789109d6a1f0e
PeaChack <17183132+PeaChack@users.noreply.github.com>
2024-02-14 13:56:19 +0300

    some changes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const button = document.getElementById("escapeButton");
 
-
-button.addEventListener("mouseover", (e) =>{
-	const button = document.getElementById("escapeButton");
-	console.log(button.style);
-	let x = window.innerWidth - parseInt(button.style.width, 10);
-	console.log(x);
+button.addEventListener("mouseover", (e) => {
+	let buttonWidth = parseInt(getComputedStyle(button).width, 10);
+	let buttonHeight = parseInt(getComputedStyle(button).height, 10);
+	let x = getRand(buttonWidth / 2, window.innerWidth - buttonWidth);
+	let y = getRand(0, window.innerHeight - buttonHeight);
+	console.log("x = " + x);
+	console.log("y = " + y);
 	button.style.left = x + 'px';
+	button.style.top = y + 'px';
 });
+
+function getRand(min, max) {
+	return Math.floor(Math.random() * (max - min) + min);
+}
