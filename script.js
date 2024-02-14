2d42fe172eab2cf01af29d15824182c6f70db922
PeaChack <17183132+PeaChack@users.noreply.github.com>
2024-02-14 16:58:42 +0300

    final

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-const button = document.getElementById("noButton");
+const noButton = document.getElementById("noButton");
+const yesButton = document.getElementById("yesButton");
+const centerImg = document.getElementById("center-img");
+let yesPressedCounter = 0;
 
-button.addEventListener("mouseover", (e) => {
-	let buttonWidth = parseInt(getComputedStyle(button).width, 10);
-	let buttonHeight = parseInt(getComputedStyle(button).height, 10);
+noButton.addEventListener("mouseover", (e) => {
+	let buttonWidth = parseInt(getComputedStyle(noButton).width, 10);
+	let buttonHeight = parseInt(getComputedStyle(noButton).height, 10);
+	let prevX =  parseInt(getComputedStyle(noButton).left, 10)
+	let prevY =  parseInt(getComputedStyle(noButton).top, 10)
 	let x = getRand(buttonWidth / 2, window.innerWidth - buttonWidth);
 	let y = getRand(0, window.innerHeight - buttonHeight);
-	console.log("x = " + x);
-	console.log("y = " + y);
-	button.style.left = x + 'px';
-	button.style.top = y + 'px';
+	console.log(window.innerWidth + ',' + window.innerHeight)
+	console.log(prevX + ',' + prevY);
+	console.log(x + ',' + y);
+	noButton.style.left = x + 'px';
+	noButton.style.top = y + 'px';
 });
 
 function getRand(min, max) {
 	return Math.floor(Math.random() * (max - min) + min);
 }
+
+yesButton.addEventListener("click", (e) =>{
+	yesPressedCounter++;
+	if(yesPressedCounter == 1){
+		let text = document.getElementById("text");
+		text.innerHTML = "Так будем мы вместе, так будем мы рядом<br>с тобою всегда!"
+		centerImg.setAttribute("src", "./resources/bear-kiss-bear-kisses.gif");
+	}
+})
